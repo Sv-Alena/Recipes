41c7b537b40cda097a77ef99f6b5d78082448bde
Elena Svyatoshenko <lena.svyatoshenko@mail.ru>
2023-01-31 12:03:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import video from './food.mp4'
+import ListOfRecipes from './ListOfRecipes';
 
 function App() {
+
+  const API_ID ='abfb5e50';
+  const API_KEY = 'dbe3619ca2a2e76e3e270e12cda037fe'
+
+  const [mySerch, setMySerch]= useState('');
+  const [myRecipes, setMyRecipes]= useState([])
+  const [word, setWord] = useState('fish')
+
+  useEffect(()  => {
+   recipesAPI()
+  },[word])
+
+  const recipesAPI = async() =>{ 
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${word}&app_id=${API_ID}&app_key=${API_KEY}`)
+    const data = await response.json()
+    setMyRecipes(data.hits);
+  }
+
+  const searchRecipes= (e) =>{
+    setMySerch(e.target.value)
+  }
+
+  const stopSearch = (e) =>{
+    e.preventDefault()
+    setWord(mySerch)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="container">
+  <video autoPlay muted loop>
+   <source src={video} type="video/mp4" />
+</video>
+  <h1>Find a Recipe</h1>
+  </div>
+  <div className="container">
+    <form onSubmit={stopSearch}>
+      <input className='search' placeholder='search...' onChange={searchRecipes} value={mySerch}></input>
+    </form>
     </div>
+  <div className="container">
+  <button>
+      <img src='https://img.icons8.com/fluency/48/000000/fry.png' alt='search'/>
+    </button>
+  </div>
+<div>
+  {myRecipes.map(element =>(
+    <ListOfRecipes
+     label={element.recipe.label}
+     image={element.recipe.image}
+     calories={element.recipe.calories}
+     ingredient={element.recipe.ingredientLines}
+    />
+  ))}
+  </div>
+  </div>
   );
 }
 
---
+++ src/ListOfRecipes.js
@@ -0,0 +1,23 @@
+function ListOfRecipes({label, image, calories, ingredient}){
+return(
+    <div>
+        <div className='container'>
+        <h2>{label}</h2>
+        </div>
+        <div className='container'>
+        <img className="photo" src={image} alt='photoRecipes'/>
+        </div>
+        
+        <ul className='list'>
+            {ingredient.map(item =>(
+                <li><img className="icon" src='https://img.icons8.com/fluency/512/checked-radio-button.png' alt='icon'/>{item}</li>
+            ))
+            }
+        </ul>
+        <div className='container'>
+        <p className="ccal">{calories.toFixed()} calories</p>
+        </div>
+    </div>
+)
+}
+export default ListOfRecipes
